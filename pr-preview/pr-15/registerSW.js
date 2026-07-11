07462114a1fbbc4741dd8fa262664a210a8fa9c3
opariffazman <opariffazman@users.noreply.github.com>
2026-07-11 02:37:25 +0000

    Deploy preview for PR 15 🛫

---
+++ pr-preview/pr-15/registerSW.js
@@ -0,0 +1,1 @@
+if('serviceWorker' in navigator) {window.addEventListener('load', () => {navigator.serviceWorker.register('/gitverse/pr-preview/pr-15/sw.js', { scope: '/gitverse/pr-preview/pr-15/' })})}
