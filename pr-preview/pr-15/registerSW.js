0a256d1f3a54e34338d9bceba571b65f4f9b2e40
opariffazman <opariffazman@users.noreply.github.com>
2026-07-11 14:36:29 +0000

    Remove preview for PR 15 🛬

--- pr-preview/pr-15/registerSW.js
+++
@@ -1,1 +0,0 @@
-if('serviceWorker' in navigator) {window.addEventListener('load', () => {navigator.serviceWorker.register('/gitverse/pr-preview/pr-15/sw.js', { scope: '/gitverse/pr-preview/pr-15/' })})}
